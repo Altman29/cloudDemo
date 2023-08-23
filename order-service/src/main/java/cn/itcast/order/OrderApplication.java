ae5315cd50d0872903ac8dd81a5497397f6a28e5
ChenBuchuo <s1yuan_chen@163.com>
2023-08-23 18:20:08 +0800

    feign 相关

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,14 +1,18 @@
 package cn.itcast.order;
 
+import org.example.feign.clients.UserClient;
+import org.example.feign.config.DefaultFeignConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
+@EnableFeignClients(clients = UserClient.class, defaultConfiguration = DefaultFeignConfiguration.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
@@ -17,7 +21,7 @@ public class OrderApplication {
 
     @Bean
     @LoadBalanced //负载均衡
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,10 +2,10 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import cn.itcast.order.pojo.User;
+import org.example.feign.clients.UserClient;
+import org.example.feign.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -14,18 +14,28 @@ public class OrderService {
     private OrderMapper orderMapper;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
-    public Order queryOrderById(Long orderId) {
-        // 1.查询订单
+    public Order queryOrderById(Long orderId){
         Order order = orderMapper.findById(orderId);
-        // 利用RestTemplate发起http请求，查询用户
-//        String url="http://localhost:8081/user/"+order.getUserId();
-        // 使用eureka 服务名称访问
-        String url="http://UserService/user/"+order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        User user = userClient.findById(order.getUserId());
         order.setUser(user);
-        // 4.返回
         return order;
     }
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    public Order queryOrderById(Long orderId) {
+//        // 1.查询订单
+//        Order order = orderMapper.findById(orderId);
+//        // 利用RestTemplate发起http请求，查询用户
+////        String url="http://localhost:8081/user/"+order.getUserId();
+//        // 使用eureka 服务名称访问
+//        String url="http://UserService/user/"+order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+//        order.setUser(user);
+//        // 4.返回
+//        return order;
+//    }
 }
