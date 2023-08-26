cc382534480f50eb81344e39f43be6ae5478f3a6
Chen <s1yuan_chen@163.com>
2023-08-26 22:35:02 +0800

    fix

--- feign-api/src/main/java/org/example/feign/pojo/User.java
+++ feign-api/src/main/java/org/example/feign/pojo/User.java
@@ -7,4 +7,32 @@ public class User {
     private Long id;
     private String username;
     private String address;
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            System.out.println("t1 start");
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("t2 start");
+        });
+
+        Thread t3 = new Thread(() -> {
+            try {
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("t3 start");
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
